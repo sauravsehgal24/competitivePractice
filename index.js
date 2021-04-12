cbfd04ae22924cb34f448c61acda00323d01718f
sauravsehgal24 <sauravsehgal44@gmail.com>
2021-04-11 20:47:28 -0400

    lis working

--- index.js
+++ index.js
@@ -85,8 +85,100 @@ const attemptsToTraverse2DMatrix = (row, col, memo = {}) => {
   return memo[row][col];
 };
 
+// ARRAY OF INCREASING SUB-SEQUENCES
+const increasingSubSeqs = (arr, n = 0, vals = []) => {
+  if (vals[n]) {
+    return vals[n];
+  }
+  if (arr.length - 1 === n) {
+    vals[n] = [arr[n]];
+    return vals[n];
+  }
+  for (var j = n + 1; j < arr.length; j++) {
+    if (arr[n] < arr[j]) {
+      vals[n] = [arr[n], ...increasingSubSeqs(arr, j, vals)];
+      if (n === 0) return vals;
+      return vals[n];
+    } else {
+      vals[n === 0 ? 0 : n] = [
+        arr[n],
+        ...increasingSubSeqs(arr, n === 0 ? 1 : j, vals),
+      ];
+    }
+  }
+  return vals;
+};
+
+// LONGEST INCREASING SUB-SEQUENCE
+const longestIncreasingSubSeq = (
+  arr,
+  obj = { vals: [], maxlength: 0 },
+  _n = 0
+) => {
+  console.log(obj);
+  if (_n === arr.length - 1) {
+    obj.vals[_n] = 1;
+    return obj;
+  }
+  for (var n = _n; n < arr.length; n++) {
+    if (obj && obj.vals && obj.vals[n]) return obj;
+    for (var j = n + 1; j < arr.length; j++) {
+      if (arr[n] < arr[j]) {
+        const r = 1 + lengthIncreasingSubSeqs(arr, obj, j).vals[j];
+        console.log(r);
+        if (!obj.vals[n] || obj.vals[n] < r) {
+          obj.vals[n] = r;
+        }
+      } else {
+        if (!obj.vals[n]) {
+          obj.vals[n] = 1;
+        }
+      }
+    }
+  }
+  return obj;
+};
+
+// ARRAY OF LENGTH OF INCREASING SUB-SEQUENCES
+const lengthIncreasingSubSeqs = (arr, vals = [], _n = 0) => {
+  if (_n === arr.length - 1) {
+    vals[_n] = 1;
+    return vals;
+  }
+  for (var n = _n; n < arr.length; n++) {
+    if (vals[n]) return vals;
+    for (var j = n + 1; j < arr.length; j++) {
+      if (arr[n] < arr[j]) {
+        const r = 1 + lengthIncreasingSubSeqs(arr, vals, j)[j];
+        if (!vals[n] || vals[n] < r) {
+          vals[n] = r;
+        }
+      } else {
+        if (!vals[n]) {
+          vals[n] = 1;
+        }
+      }
+    }
+  }
+  return vals;
+};
+
 // PRACTICE QUESTIONS
 //https://www.codingame.com/playgrounds/5422/js-interview-prep-recursion
+const lisArray = [4, 7, 5, 6, 8, 9];
+// console.log(`\n--------------------------------------------------------`);
+// const lis = longestIncreasingSubSeq(lisArray);
+// console.log(`Length of longest increasing sub-sequence = ${lis.vals}`);
+
+// console.log(`\n--------------------------------------------------------`);
+// const increasingSubSeqsVal = increasingSubSeqs(lisArray);
+// console.log(`Array of Increasing Sub Sequences in Array = `);
+// console.log(increasingSubSeqsVal);
+
+console.log(`\n--------------------------------------------------------`);
+const arraylengthISeq = lengthIncreasingSubSeqs(lisArray);
+console.log(`Array of Lengths of Increasing Sub Sequences in Array = `);
+console.log(arraylengthISeq);
 
 console.log(`\n--------------------------------------------------------`);
 //const mdArrtay = [[3, [5]], 0, 2, ["foo"], [], [4, [5, 6]]];
